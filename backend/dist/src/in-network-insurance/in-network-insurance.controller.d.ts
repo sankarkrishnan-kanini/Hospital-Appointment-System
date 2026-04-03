24b234a44978ae663bd3cb9336250c0cbce6287e
Sankar <sankarkrishnan2002@gmail.com>
2026-04-03 11:17:13 +0530

    final business logic addition in Noestjs backend

--- backend/dist/src/in-network-insurance/in-network-insurance.controller.d.ts
+++ backend/dist/src/in-network-insurance/in-network-insurance.controller.d.ts
@@ -8,55 +8,55 @@ export declare class InNetworkInsuranceController {
         doctorHospital: {
             id: number;
             doctorId: number;
-            city: string | null;
-            country: string | null;
-            timeSlotPerClientInMin: number;
-            firstConsultationFee: number;
-            followupConsultationFee: number;
-            streetAddress: string | null;
-            state: string | null;
-            zip: string | null;
             hospitalId: number | null;
             isPrivate: boolean;
+            streetAddress: string | null;
+            city: string | null;
+            state: string | null;
+            country: string | null;
+            zip: string | null;
+            firstConsultationFee: number;
+            followupConsultationFee: number;
+            timeSlotPerClientInMin: number;
         };
     } & {
         id: number;
-        insuranceName: string;
         doctorHospitalId: number;
+        insuranceName: string;
     })[]>;
     findOne(id: number): Promise<{
         doctorHospital: {
             id: number;
             doctorId: number;
-            city: string | null;
-            country: string | null;
-            timeSlotPerClientInMin: number;
-            firstConsultationFee: number;
-            followupConsultationFee: number;
-            streetAddress: string | null;
-            state: string | null;
-            zip: string | null;
             hospitalId: number | null;
             isPrivate: boolean;
+            streetAddress: string | null;
+            city: string | null;
+            state: string | null;
+            country: string | null;
+            zip: string | null;
+            firstConsultationFee: number;
+            followupConsultationFee: number;
+            timeSlotPerClientInMin: number;
         };
     } & {
         id: number;
-        insuranceName: string;
         doctorHospitalId: number;
+        insuranceName: string;
     }>;
     create(userId: number, dto: CreateInNetworkInsuranceDto): Promise<{
         id: number;
-        insuranceName: string;
         doctorHospitalId: number;
+        insuranceName: string;
     }>;
     update(userId: number, id: number, dto: UpdateInNetworkInsuranceDto): Promise<{
         id: number;
-        insuranceName: string;
         doctorHospitalId: number;
+        insuranceName: string;
     }>;
     remove(userId: number, id: number): Promise<{
         id: number;
-        insuranceName: string;
         doctorHospitalId: number;
+        insuranceName: string;
     }>;
 }
