5428ac834152bc25f8629743a49fb261b0a78377
Mounika Bommanchenu <mounika.bommanchenu@kanini.com>
2026-04-15 12:53:14 +0530

    not exposing secrets

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -10,7 +10,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
 			host: process.env.DB_HOST ?? '127.0.0.1',
 			user: process.env.DB_USER ?? 'root',
-			password: process.env.DB_PASSWORD ?? '',
+			password: process.env.DB_PASSWORD ,
 			database: process.env.DB_NAME ?? 'hospitalmanagementdb',
 			allowPublicKeyRetrieval: true
 
